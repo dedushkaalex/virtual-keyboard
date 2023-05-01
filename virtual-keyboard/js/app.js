393c424894e4b8b0ee8d8188807ab05f480737a2
Aleksei Erchenko <proger@a1exxuss.ru>
2023-05-01 23:39:32 +0300

    docs: add description on which system the keyboard was executed

--- virtual-keyboard/js/app.js
+++ virtual-keyboard/js/app.js
@@ -3,6 +3,7 @@ import Keyboard from './components/Keyboard.js';
 document.body.insertAdjacentHTML('beforeend', `
 <div class= "help" >
   <ul class="help__list">
+    <li class="help__item">КЛАВИАТУРА ВЫПОЛНЕНА НА СИСТЕМЕ <strong>WINDOWS</strong></li>
     <li class="help__item">Переключение раскладки по клавишам <strong>ctrl + alt</strong> ( левые клавиши )</li>
     <li class="help__item">Смена языка сохраняется после перезагрузки страницы</li>
   </ul>
